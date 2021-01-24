182b6b679117fcd0bc206de35da04f2187f1d3ae
Aibee <iboroinyang01@gmail.com>
2021-01-24 19:56:36 +0100

    Added learn more button to top nav

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
         <router-link to="tests" class="text-dark px-3">Tests </router-link>
+        <router-link to="schools" class="text-dark px-3">Find School</router-link>
         <router-link to="learning" class="text-dark px-3">Learn More</router-link>
       </b-navbar-nav>
 
@@ -40,6 +41,10 @@ export default {
 #app{
   background-image: linear-gradient(to right, #fff, #f8f9fb, #dcdfe4, #a0a1a3);
   font-family: 'Ubuntu', sans-serif !important;
+}
+a.router-link-active{
+  color: #232525;
+  font-weight: bolder;
 }
 a.navbar-brand>strong{
   transition: all 0.7s ease-out;
